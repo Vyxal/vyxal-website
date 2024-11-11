ad2f0dff8f09b1e85523f161235325a9db778d44
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-11-11 13:28:17 -0500

    Use msgpack for permalinks instead of JSON

--- src/latest/scripts/interpreter/permalink.ts
+++ src/latest/scripts/interpreter/permalink.ts
@@ -1,5 +1,6 @@
 import compatRaw from "../../data/compat.json?raw";
-import { gunzipString, gzipString } from "gzip-utils";
+import { gunzipString, gzipUint8Array } from "gzip-utils";
+import { pack, unpack } from "msgpackr";
 
 const compat = JSON.parse(compatRaw);
 
@@ -29,7 +30,9 @@ function incompatible(permalinkVersion: string) {
 }
 
 export function encodeHash(permalink: Omit<Permalink, "format">): Promise<string> {
-    return gzipString(JSON.stringify({ format: latestPermalink, ...permalink } as Permalink), "base64url") as Promise<string>;
+    // probably not great for performance but whatever
+    const buf = new Uint8Array([0xff, ...pack({ format: latestPermalink, ...permalink } as Permalink)]);
+    return gzipUint8Array(buf, "base64url") as Promise<string>;
 }
 
 type DecodeResult = {
@@ -43,7 +46,15 @@ type DecodeResult = {
 export async function decodeHash(hash: string): Promise<DecodeResult | null> {
     let permalink: OldPermalinks | Permalink;
     try {
-        permalink = JSON.parse(await gunzipString(hash, "base64url", "utf8") as string);
+        // First try compressed permalinks
+        const decompressed = await gunzipString(hash, "base64url", "raw") as Uint8Array;
+        if (decompressed[0] == 0xff) {
+            // msgpack
+            permalink = unpack(decompressed.slice(1));
+        } else {
+            // JSON
+            permalink = JSON.parse(new TextDecoder().decode(decompressed));
+        }
     } catch (decompressError) {
         // Try to decode it as a non-compressed permalink
         try {
